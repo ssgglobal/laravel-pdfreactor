95b7a72ae7f1b4af3b133ea37252a9c9b7e1604b
Jess Carlos <jcarlos@stepstoneglobal.com>
2021-03-05 18:02:03 -0800

    config stub

---
+++ config/services.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'pdfreactor'    => [
+        'host'  => env('PDFREACTOR_HOST'),
+        'port'  => env('PDFREACTOR_PORT', 9243),
+        'key'   => env('PDFREACTOR_KEY'),
+    ],
+
+];
